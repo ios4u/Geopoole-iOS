6e552548ef3913417d199c9f237eabf713569e05
James Kolsby <jrkolsby@mac.com>
2014-04-18 16:48:59 -0400

    Initial Commit

--- Geopoole/AppDelegate.m
+++ Geopoole/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent]; // Light status bar
     return YES;
 }
 							
